429c3b5461227083dd04f5dbd788b5e2793a4ce5
Aija Mezapuke <aija.mezapuke@gmail.com>
2021-09-16 18:57:24 +0200

    Skelet of Portfolio page

--- main.go
+++ main.go
@@ -1,26 +1,90 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
-	"os"
-
-	"github.com/ForestFlowerr/portfolio/app/controllers"
 )
 
+var tpl *template.Template
+
+type pageData struct {
+	Title     string
+	FirstName string
+}
+
+func init() {
+	tpl = template.Must(template.ParseGlob("app/views/templates/*.gohtml"))
+}
+
 func main() {
+	http.HandleFunc("/", idx)
+	http.HandleFunc("/about", abot)
+	http.HandleFunc("/contact", cntct)
+	http.HandleFunc("/apply", aply)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
 
-	// Print something on startup
-	fmt.Println("Starting HTTP server!")
-	port := os.Getenv("PORT")
-	fmt.Println("Running on PORT::", port)
+func idx(w http.ResponseWriter, req *http.Request) {
 
-	hello := controllers.Hello
+	pd := pageData{
+		Title: "Index Page",
+	}
 
-	// Routes
-	// Print msg "Hello, world!" or "Hello, URL!"
-	http.HandleFunc("/", hello)
+	err := tpl.ExecuteTemplate(w, "index.gohtml", pd)
+	if err != nil {
+		log.Println("LOGGED", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
 
-	// Start server
-	http.ListenAndServe(":"+port, nil)
+}
+
+func abot(w http.ResponseWriter, req *http.Request) {
+
+	pd := pageData{
+		Title: "About Page",
+	}
+	err := tpl.ExecuteTemplate(w, "about.gohtml", pd)
+	if err != nil {
+		log.Println("LOGGED", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+}
+
+func cntct(w http.ResponseWriter, req *http.Request) {
+
+	pd := pageData{
+		Title: "Contact Page",
+	}
+
+	err := tpl.ExecuteTemplate(w, "contact.gohtml", pd)
+	if err != nil {
+		log.Println("LOGGED", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+}
+
+func aply(w http.ResponseWriter, req *http.Request) {
+
+	pd := pageData{
+		Title: "Apply Page",
+	}
+
+	var first string
+	if req.Method == http.MethodPost {
+		first = req.FormValue("fname")
+		pd.FirstName = first
+	}
+	err := tpl.ExecuteTemplate(w, "apply.gohtml", pd)
+	if err != nil {
+		log.Println("LOGGED", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
 }
